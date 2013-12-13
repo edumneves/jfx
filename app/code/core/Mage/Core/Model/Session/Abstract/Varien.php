5f2fa5b9809541734de037394863379457483285
edumneves <edumneves@gmail.com>
2013-12-12 23:48:50 -0200

    #### AJUSTE NO CORE ###### Para funcionar no chrome

--- app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
+++ app/code/core/Mage/Core/Model/Session/Abstract/Varien.php
@@ -85,6 +85,7 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
         }
 
         // session cookie params
+/* Edu - para funcionar no chrome
         $cookieParams = array(
             'lifetime' => $cookie->getLifetime(),
             'path'     => $cookie->getPath(),
@@ -92,6 +93,7 @@ class Mage_Core_Model_Session_Abstract_Varien extends Varien_Object
             'secure'   => $cookie->isSecure(),
             'httponly' => $cookie->getHttponly()
         );
+*/
 
         if (!$cookieParams['httponly']) {
             unset($cookieParams['httponly']);
